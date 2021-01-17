f799f70e8a5b97e5886271c0bd591ad17f9dea6a
Spoof-UoS <saudko-1@naver.com>
2021-01-17 14:42:29 +0900

    add 7.py

--- solutions/7/7.py
+++ solutions/7/7.py
@@ -2,9 +2,9 @@ class Solution(object):
     def reverse(self, x):
         if x < 0:
             sign = -1
-        elif x == 0 or abs(x-0.5)>(2**31 - 0.5:
-            return 0
         else:
             sign = 1
         rev = int(str(abs(x))[::-1])*sign
+        if abs(rev-0.5)>(2**31 - 0.5):
+            return 0
         return rev
