3b35c3912918fdc1be90e7c19bf586358fceb412
Spoof-UoS <saudko-1@naver.com>
2021-01-19 23:09:24 +0900

    add 172.py

--- solutions/172/172.py
+++ solutions/172/172.py
@@ -1,10 +1,12 @@
-n = 5
-
-from math import log
-arr = []
-answer = 0
-for i in range(1,int(log(n)/log(5)+1)):
-    arr.append(5**i)
-for j in range(len(arr)):
-    answer += n//arr[j]
-print(answer)
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        from math import log
+        if n == 0:
+            return 0
+        arr = []
+        answer = 0
+        for i in range(1,int(log(n)/log(5)+1)):
+            arr.append(5**i)
+        for j in range(len(arr)):
+            answer += n//arr[j]
+        return answer
