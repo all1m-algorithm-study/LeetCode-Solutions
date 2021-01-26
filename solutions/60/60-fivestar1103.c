9ab79d4a73c7c167075fbc7f38f3a0b814201dd0
fivestar1103 <fivestar1103@uos.ac.kr>
2021-01-26 16:42:31 +0900

    add 52-fivestar1103.py

--- solutions/60/60-fivestar1103.c
+++ solutions/60/60-fivestar1103.c
@@ -4,23 +4,25 @@ int factorial(int f) {
 	return ret;
 }
 
-char* getPermutation(int n, int k) {
-	int tmp = 0, idx, i, j;
-	char* ret = malloc((n + 1) * sizeof(char));
-	int* notUsed = malloc((n + 1) * sizeof(int));
-	for (i = 1; i <= n; i++) notUsed[i] = i;
+char * getPermutation(int n, int k){
+    int tmp = 0, idx, i, j;
+    char* ret = malloc((n + 1) * sizeof(char));
+	int* used = calloc(n + 1, sizeof(int)); 
 	k--;
 	for (i = n - 1; i >= 1; i--) {
 		idx = 0;
 		for (j = 0; j <= k / factorial(i); j++) {
 			idx++;
-			while (!notUsed[idx]) idx++;
+			while (used[idx]) idx++;
 		}
 		tmp = tmp * 10 + idx;
-		notUsed[idx] = 0;
+		used[idx] = 1;
 		k %= factorial(i);
 	}
-	for (i = 1; i <= n; i++) if (notUsed[i]) tmp = tmp * 10 + i;
-	sprintf(ret, "%d", tmp);
-	return ret;
+	for (i = 1; i <= n; i++) if (!used[i]) {
+        tmp = tmp * 10 + i;
+        break;
+    }
+    sprintf(ret, "%d", tmp);
+    return ret;
 }
