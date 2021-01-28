7a316e54891a2f62bc6d58ee7b86a88c63c52bd3
kir3i <kirei7081@gmail.com>
2021-01-28 11:45:12 +0900

    modify 1315-kir3i.py, 1315-kir3i.md

--- solutions/1315/1315-kir3i.py
+++ solutions/1315/1315-kir3i.py
@@ -8,17 +8,17 @@ class Solution:
     def sumEvenGrandparent(self, root):
         ans = 0
 
-        #BFS
-        q = [(root, False)]
-        while q:
-            curr, isParentEven = q.pop()
+        #DFS
+        s = [(root, False)]
+        while s:
+            curr, isParentEven = s.pop()
 
             if curr.left:
-                q.append((curr.left, curr.val % 2 == 0))
+                s.append((curr.left, curr.val % 2 == 0))
                 if isParentEven:
                     ans += curr.left.val
             if curr.right:
-                q.append((curr.right, curr.val % 2 == 0))
+                s.append((curr.right, curr.val % 2 == 0))
                 if isParentEven:
                     ans += curr.right.val
         return ans
