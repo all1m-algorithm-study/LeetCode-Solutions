106a0e5834f9d7810288d2fcda15263ed192425e
hyun gyu <gusrb3164@gmail.com>
2021-02-17 00:11:06 +0900

    add 226-gusrb3164.js

---
+++ solutions/226/226-gusrb3164.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+	if (root === null) return null;
+
+	let tmp = root.right;
+	root.right = invertTree(root.left);
+	root.left = invertTree(tmp);
+
+	return root;
+};
